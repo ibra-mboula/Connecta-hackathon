1289a1da007182f57869dc5c0e56025ec6c35900
ibrahim <mboulaibrahim@gmail.com>
2024-03-16 16:46:55 +0100

    feat: added mongo db

--- app/server.js
+++ app/server.js
@@ -1,25 +1,49 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = 3000;
 
-// Serveur les fichiers statiques du dossier public
+const uri = "mongodb+srv://52676:yNHdKFyn3tPVtptZ@cluster0.kswno1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: ServerApiVersion.v1 
+});
+
 app.use(express.static('public'));
 
-// Route pour la page Home
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/home.html');
 });
 
-// Route pour la page Forum
 app.get('/forum', (req, res) => {
   res.sendFile(__dirname + '/views/forum.html');
 });
 
-// Route pour la page Carte
 app.get('/carte', (req, res) => {
   res.sendFile(__dirname + '/views/carte.html');
 });
 
-app.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
-});
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+
+    const database = client.db('hack_data');
+    const collection = database.collection('data');
+
+    await collection.insertOne({ message: "Initialization document.." });
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // Uncomment the following line if you want to close the database connection after the server stops
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
