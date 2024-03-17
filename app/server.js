06c93201496a1c8b6083ea0c60b2e8f006c07114
ibrahim <mboulaibrahim@gmail.com>
2024-03-17 12:45:45 +0100

    feat: propto v1

--- app/server.js
+++ app/server.js
@@ -1,27 +1,99 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const path = require('path'); // Module pour manipuler les chemins de fichiers
 const app = express();
 const port = 3000;
 
-const uri = "mongodb+srv://52676:yNHdKFyn3tPVtptZ@cluster0.kswno1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Mise à jour de l'URI pour utiliser MongoDB Atlas
+//const uri = "mongodb+srv://52676:yNHdKFyn3tPVtptZ@cluster0.kswno1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true&authSource=admin";
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
 
-const client = new MongoClient(uri, {
-  serverApi: ServerApiVersion.v1 
-});
+app.set('view engine', 'ejs'); // Configuration du moteur de modèle EJS
+app.set('views', path.join(__dirname, 'views')); // Spécification du répertoire des vues
 
+
+app.use(express.json());
 app.use(express.static('public'));
+//app.use(express.static('views'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/profil', (req, res) => {
+  res.sendFile(__dirname + '/views/profil.html');
 });
 
 app.get('/forum', (req, res) => {
   res.sendFile(__dirname + '/views/forum.html');
 });
 
-app.get('/carte', (req, res) => {
-  res.sendFile(__dirname + '/views/carte.html');
+app.get('/data', (req, res) => {
+  res.sendFile(__dirname + '/views/data.html');
+});
+
+app.get('/api/data', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('hack_data');
+    const collection = database.collection('data2');
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+app.get('/api/statistics', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('hack_data');
+    const collection = database.collection('data2');
+    
+    // Remplacez ceci par la logique appropriée pour récupérer et formater les données de votre collection
+    const statistics = await collection.aggregate([
+      { $group: { _id: "$arrondissement", count: { $sum: 1 } } }
+    ]).toArray();
+
+    res.json(statistics);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+app.get('/carte', async (req, res) => {
+
+  await client.connect();
+  console.log("Connected successfully to MongoDB");
+
+  const database = client.db('hack_data');
+  const collection = database.collection('data2');
+
+  data = await collection.find({}).toArray();
+
+  data.forEach(element => {
+    element.coord.long = parseFloat(element.coord.long)
+    element.coord.lat = parseFloat(element.coord.lat)
+  });
+
+  commune_detected = {}
+
+  data.forEach(element=>{
+    if (element.commune in commune_detected){
+      commune_detected[element.commune] += 1
+    }else{
+      commune_detected[element.commune] = 1
+    }
+  });
+
+  data.forEach(item=>{
+    if(item.commune in commune_detected){
+      item.number = commune_detected[item.commune]
+    }
+  });
+
+  res.render('carte', { data: data }); // Rendre la page carte.ejs
 });
 
 async function run() {
@@ -30,9 +102,10 @@ async function run() {
     console.log("Connected successfully to MongoDB");
 
     const database = client.db('hack_data');
-    const collection = database.collection('data');
+    const collection = database.collection('data2');
 
-    await collection.insertOne({ message: "Initialization document.." });
+    data_coord = await collection.find({}).toArray();
+    
 
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
@@ -47,3 +120,4 @@ async function run() {
 }
 
 run().catch(console.dir);
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 from pymongo import MongoClient
 import time
+import certifi
+
 
 LIST_NAME = []
 LIST_PHONE_NUMBER = []
@@ -19,11 +21,12 @@ def calculate_senior_isolated_population(senior_file_path, population_data):
     with open(senior_file_path, 'r', encoding='utf-8') as file:
         for line in file:
             line_data = line.strip().split(';')
-            senior_isolated_indicator[line_data[2]] = line_data[3].replace(',', '.')  # Remplacer la virgule par un point
+            if line_data[1] == "Commune":
+                senior_isolated_indicator[line_data[2]] = {"indicator" : line_data[3].replace(',', '.'), "type" : line_data[1]}
 
     for senior in senior_isolated_indicator:
         if senior in population_data:
-            senior_isolated_population[senior] = int(population_data[senior] * float(senior_isolated_indicator[senior]))
+            senior_isolated_population[senior] = {"pop_alone" : int(population_data[senior] * float(senior_isolated_indicator[senior]['indicator'])) , "type" : senior_isolated_indicator[senior]["type"]}
     return senior_isolated_population
 
 def set_random_value():
@@ -62,33 +65,70 @@ def set_coord(path_to_csv, senior_isolated_population):
 def insert_document(collection, data_to_insert):
     collection.insert_one(data_to_insert)
 
+def get_municipality(filename):
+    regions = {}
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            line_parsed = line.strip().split(';')
+            type_entity = line_parsed[1]
+            name = line_parsed[2]
+            
+            if type_entity == 'Province':
+                province = name
+                regions[province] = {}
+            elif type_entity == 'Arrondissement':
+                arrondissement = name
+                regions[province][arrondissement] = []
+            elif type_entity == 'Commune':
+                regions[province][arrondissement].append(name)
+    return regions            
+            
 if __name__ == "__main__":
     population_file_path = '200300.csv'
     senior_file_path = '244400.csv' 
     set_random_value()
     population_data = parse_population(population_file_path)
     senior_isolated_population = calculate_senior_isolated_population(senior_file_path, population_data)
-    
+
     data_to_send = set_coord('communes-belges0.csv',senior_isolated_population)
     
-    
+    city_per_mun = get_municipality('244400.csv')
+
     while True:
         random_value = random.randint(0,len(data_to_send)-1)
         random_profil = data_to_send[random_value]
-        uri = "mongodb://localhost:27017"
-        client = MongoClient(uri)
-        
+        #uri = "mongodb+srv://52676:yNHdKFyn3tPVtptZ@cluster0.kswno1o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true&authSource=admin"
+        uri= "mongodb://127.0.0.1:27017"
+        client = MongoClient(uri)        
         try:
-            # Connexion à la base de données MongoDB
+            # Connexion à MongoDB
             print("Connecting to MongoDB...")
             db = client.hack_data
-            collection = db.data
+            collection = db.data2
 
-            # Insertion du document dans la collection
+            # Je définir l'arrondissement et la province
+            commune_to_found = random_profil['commune']
+            arrondissement = None
+            Province = None
+            
+            for prov in city_per_mun:
+                try:
+                    for arrond in city_per_mun[prov]:
+                        if commune_to_found in city_per_mun[prov][arrond]:
+                            arrondissement = arrond
+                            Province = prov
+                except:
+                    pass
+                
+            random_profil['arrondissement'] = arrondissement
+            random_profil['province'] = Province
+
+
+            # Insertion collection
             insert_document(collection, random_profil)
             print("Document inserted successfully.")
             
-            time.sleep(10)
+            time.sleep(1)
 
         except Exception as e:
             print("An error occurred:", e)
